41e8052945e67d7b30fb4913ecaed3bdface8a0f
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-01 18:46:32 +0200

    Props

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ class HeaderComponent extends React.Component {
     }
 
     render() { // єдиний метод, який ви зобов'язані визначити в класі
+        console.log(this.props);
         const p = React.createElement('p', {}, 'Super paragraph');
-        const h2 = React.createElement('h2', {title: 'Hi!'}, 'Header');
+        const h2 = React.createElement('h2', {title: 'Hi!'}, `Hello ${this.props.name}`);
         const article = React.createElement('article', {}, h2, p);
         return article;
     }
 }
 
-const component = React.createElement(HeaderComponent);
+const component = React.createElement(HeaderComponent, {name: 'John'});
+const component2 = React.createElement(HeaderComponent, {name: 'Oleg'});
+
+const parentElement = React.createElement('section', {}, component, component2);
 
 
 const root = document.querySelector('#root');
 
-ReactDOM.render(component, root);
+ReactDOM.render(parentElement, root);
