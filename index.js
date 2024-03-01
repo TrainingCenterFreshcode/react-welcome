4a9dc9870ac307635808ab05ad0305c015209bb7
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-01 19:10:55 +0200

    Add prop children + conspectus

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ class HeaderComponent extends React.Component {
     }
 }
 
-const component = React.createElement(HeaderComponent, {name: 'John'});
-const component2 = React.createElement(HeaderComponent, {name: 'Oleg'});
+const component = React.createElement(HeaderComponent, {name: 'John'}, 'Text -- John');
+const component2 = React.createElement(HeaderComponent, {name: 'Oleg'}, 'Text -- Oleg');
 
 const parentElement = React.createElement('section', {}, component, component2);
 
@@ -21,3 +21,22 @@ const parentElement = React.createElement('section', {}, component, component2);
 const root = document.querySelector('#root');
 
 ReactDOM.render(parentElement, root);
+
+
+/*
+
+
+1. Реакт - бібілотека для створення користувацьких інтерфейсів
+2. Ви можете використовувати як звичайний "ванільний" JS, так можете використовувати і Реакт. Це може відбуватись одночасно
+3. Пропси - це дані, які можна передавати в компоненти для того, щоб налаштовувати зовнішній вигляд компонент.
+4. Для того, щоб Реакт нам щосб відобразив, нам потрібно викликати метод ReactDOM.render().
+Він приймає 2 параметри:
+- елемент, який потрібно відрендерити
+- елемент, в який потрібно рендерити 
+5. Реакт оперує Реакт-елементами.
+Реакт-елемент - це об'єкт.
+Реакт-елемент набагато легший, ніж той самий елемент у нативному DOM'і.
+6. З цих Реакт-елементів як маленьких блоків складаються Реакт-компоненти.
+А з Реакт-компонент складаються сторінки, які будуть бачити наші користувачі.
+
+*/
