77d435c9b78a6d28765f894443fa82109270228c
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-03-01 18:35:49 +0200

    Class Component

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+class HeaderComponent extends React.Component {
+    anotherMethod() { // інші якісь методи всередині компоненти - не заборонено
+
+    }
+
+    render() { // єдиний метод, який ви зобов'язані визначити в класі
+        const p = React.createElement('p', {}, 'Super paragraph');
+        const h2 = React.createElement('h2', {title: 'Hi!'}, 'Header');
+        const article = React.createElement('article', {}, h2, p);
+        return article;
+    }
+}
+
+const component = React.createElement(HeaderComponent);
+
 
 const root = document.querySelector('#root');
 
-const h1 = React.createElement('h1', {}, 'Hello React!'); // React-element
-console.log(h1);
-
-const anotherH1 = document.createElement('h1'); // HTML-element
-console.dir(anotherH1);
-
-ReactDOM.render(h1, root);
+ReactDOM.render(component, root);
